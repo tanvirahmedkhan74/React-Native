3a407961689aedc92f7b11511600c26f03487679
Tanvir Ahmed Khan <tanvirahmedkhan74@gmail.com>
2023-09-25 11:11:36 +0600

    Initial

---
+++ App101/AppPro.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import {
+    View,
+    Text,
+    SafeAreaView,
+    StyleSheet,
+    useColorScheme
+} from 'react-native'
+
+function AppPro(): JSX.Element{
+    return(
+        <></>
+    )
+}
+
+export default AppPro;
