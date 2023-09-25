ba84ff2af89d27036754baf094b9d07dd46a1b1d
Tanvir Ahmed Khan <tanvirahmedkhan74@gmail.com>
2023-09-25 15:01:31 +0600

    First App Work Done

--- App101/AppPro.tsx
+++ App101/AppPro.tsx
@@ -1,20 +1,50 @@
-import React from "react";
+import React from 'react';
 import {
-    View,
-    Text,
-    SafeAreaView,
-    StyleSheet,
-    useColorScheme
-} from 'react-native'
+  View,
+  Text,
+  SafeAreaView,
+  StyleSheet,
+  useColorScheme,
+  Linking
+} from 'react-native';
 
-function AppPro(): JSX.Element{
-    return(
-        <SafeAreaView>
-            <View>
-                <Text>What is Life!</Text>
-            </View>
-        </SafeAreaView>
-    )
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+
+function AppPro(): JSX.Element {                        // Return Speicification using typescript
+  
+    const isDarkMode = useColorScheme() === 'dark';       // OS color mode check
+    console.log(isDarkMode)
+    return (
+        <View style={styles.container}>
+            <Text style={isDarkMode ? styles.whiteText: styles.darkText}>
+                What is Life!
+            </Text>
+            <Text style={{color: 'red'}}>
+                This App is Way too basic. Just a milestone for reaching the Installation Properly.
+            </Text>
+            <Text style={{color: 'blue'}} 
+                onPress={() => Linking.openURL('https://tanvirahmedkhan.hashnode.dev/react-native-cli-installation-guide-for-linux-ubuntu2204')}>
+                React Native CLi Instrallation Blog
+            </Text>
+        </View>
+        
+    );
 }
 
-export default AppPro;
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center' 
+    },
+    whiteText: {
+        color: '#FFFFFF',
+        backgroundColor: '#000000'
+    },
+    darkText: {
+        color: '#000000',
+        backgroundColor: '#FFFFFF'
+    }
+});
+
+export default AppPro
